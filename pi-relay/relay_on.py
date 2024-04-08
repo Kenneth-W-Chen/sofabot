fe88441d1997b88e205381cecb239e7f8949560a
Kenneth-W-Chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2024-04-07 19:16:13 -0500

    Move deprecated code to old folder

--- pi-relay/relay_on.py
+++
@@ -1,7 +0,0 @@
-import RPi.GPIO as GPIO   # Import the GPIO library.
-import time               # Import time library
-
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(21, GPIO.OUT)
-GPIO.output(21, GPIO.LOW)
